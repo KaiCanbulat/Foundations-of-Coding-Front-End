b793f89e1f345724c89cc7f1dc21e85ee3b32d00
KaiCanbulat <kai.canbulat@gmx.de>
2025-12-18 11:53:29 +0100

    added practice program 2
    added practice program 2

---
+++ practice_program 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program {
+   public static void Main()	{
+	   
+	   //Prompt user for Account type
+	   Console.WriteLine("Enter Account type. Savings,Checking or Business");
+	   string AccountType = Console.ReadLine();
+	   
+	   switch (AccountType)
+	   {
+		   case "Savings":
+			   Console.WriteLine("Applying 2% interest rate.");
+			   break;
+			   
+		   case "Checking":
+			   Console.WriteLine("Applying a $10 montly fee.");
+			   break;
+			   
+		   case "Business":
+			   Console.WriteLine("Applying a 1% interest rate and a $20 montly fee.");
+			   break;
+			  
+		   default:
+			   Console.WriteLine("Error please choose between the options.");
+			   break;
+	   }
+   }
+}
